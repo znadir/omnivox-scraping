b90e63ef12e0d19e3521834987388937db715597
Nadir Zebiri <2266923@bdeb.qc.ca>
2023-06-11 17:32:43 -0400

    handle 2fa #1

--- src/functions/loginOmnivox.js
+++ src/functions/loginOmnivox.js
@@ -1,3 +1,5 @@
+import promptSync from "prompt-sync";
+
 /**
  * Login to Omnivox
  * Redirects page to schoolname.omnivox.ca/intr/
@@ -7,6 +9,10 @@
  * @param {string} password
  */
 export default async (page, noDA, password) => {
+	const prompt = promptSync({
+		sigint: true,
+	});
+
 	console.log("Connexion en cours...".cyan);
 
 	await page.goto(process.env.OMNIVOX_LOGIN_LINK, {
@@ -20,14 +26,67 @@ export default async (page, noDA, password) => {
 	await page.type("#Identifiant", noDA);
 	await page.type("#Password", password);
 	await page.click('button[type="submit"]');
+	await page.waitForNetworkIdle(0);
 
 	const errorLogin = await page.evaluate(
 		() => document.querySelector(".msg-erreur")?.innerText
 	);
 
 	if (errorLogin) {
-		console.log("Erreur d'authentification, veuillez réessayer".red);
 		console.log(errorLogin.red);
 		process.exit(1);
+	}
+
+	// check for 2FA
+	const is2FA = await page.evaluate(() => {
+		const divs = Array.from(document.querySelectorAll("div.MuiBox-root"));
+		return divs.filter((el) =>
+			el.innerHTML.startsWith("Un code de sécurité à 6 chiffres a été envoyé")
+		)[0]?.textContent;
+	});
+
+	if (is2FA) {
+		console.log(is2FA.yellow);
+
+		const plusDemander =
+			prompt(
+				"Tapez O pour ne plus me demander de valider mon identité sur cet appareil: "
+			) === "O";
+
+		if (plusDemander) await page.click('input[type="checkbox"]');
+
+		const is2FAIncorrect = async () => {
+			return await page.evaluate(() => {
+				const ps = Array.from(document.querySelectorAll("p"));
+				return ps.filter((el) =>
+					el.innerHTML.startsWith("Le code saisi est invalide")
+				)[0]?.textContent;
+			});
+		};
+
+		let isCodeIncorrect;
+		do {
+			let code;
+			do {
+				code = prompt("Code de sécurité (6 chiffres): ");
+				if (code.length != 6) {
+					console.log("Code invalide. Veuillez réessayer".red);
+				}
+			} while (code.length != 6);
+
+			// entrer le code 2fa
+			await page.type('input[type="text"]', code);
+
+			await page.click('button[tabindex="0"]');
+			console.log("Veuillez patienter...".cyan);
+			await page.waitForNetworkIdle(0);
+
+			isCodeIncorrect = await is2FAIncorrect();
+			if (isCodeIncorrect) console.log("Code 2FA incorrect".red);
+		} while (isCodeIncorrect);
+
+		console.log("Code 2FA correct!".green);
+	} else {
+		console.log("2FA non requis");
 	}
 };
